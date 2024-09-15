236ed490e142cb7dc0a8518dcea1a97bb19dc9ca
Angel Molina <urdhb35@gmail.com>
2024-09-14 22:54:28 -0400

    Initial commit

---
+++ mainNotes.js
@@ -0,0 +1,49 @@
+//*Congratulations for finish the last course bro, your are great and i put all my confidence in your continous progress
+
+//*Objet dom window:
+
+/*Window is the top of dom herarchy, is the object were the rest of objects depends*/
+
+
+//*window.open() method:
+
+/*This method open a new window using the link in first param:*/
+
+window.open("https://youtube.com");
+
+
+//*window.close() method:
+
+/*This method close the selected window:*/
+
+window.close();
+
+
+//*window.closed propertie:
+
+/*This a bolean propertie that is true if our window is closed or false is our window is open:*/
+
+console.log(window.closed);
+
+
+//*window.stop():
+
+/*This method let us stop the load and exucution of a page*/
+
+window.stop();
+
+
+//*window.print() method:
+
+/*This method let us print the window content*/
+
+window.print();
+
+
+//*window.confirm() method:
+
+/*This a input method that will return true if we acept or false if we don't acept:*/
+
+window.confirm();
+
+//*we stop at 13:43.
